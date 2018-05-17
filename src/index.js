5f65db956ad7594a82a128686df27fa96492e615
Alexey <asdalexey@yandex.ru>
2018-05-17 23:48:07 +0300

    #2 bridge observer and observable

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import { interval } from 'rxjs/observable/interval';
 import { take } from 'rxjs/operators/take';
 
 const observable = interval(1000).pipe(take(5));
+
 const observerA = {
   next: (x) => (console.log('A next ', x)),
   error: (err) => (console.log('A error ', err)),
   complete: () => (console.log('A done')),
 };
-observable.subscribe(observerA);
 
 const observerB = {
   next: (x) => (console.log('B next ', x)),
@@ -15,6 +15,26 @@ const observerB = {
   complete: () => (console.log('B done')),
 };
 
+const bridgeObserver = {
+  observers: [],
+  next: function (x) {
+    this.observers.forEach((o) => o.next(x));
+  },
+  error: function (err) {
+    this.observers.forEach((o) => o.error(err));
+  },
+  complete: function (err) {
+    this.observers.forEach((o) => o.complete());
+  },
+  subscribe: function (observer) {
+    this.observers.push(observer);
+  },
+};
+
+bridgeObserver.subscribe(observerA);
+observable.subscribe(bridgeObserver);
+// observable.subscribe(observerA);
+
 setTimeout(() => {
-  observable.subscribe(observerB);
+  bridgeObserver.subscribe(observerB);
 }, 2000);
