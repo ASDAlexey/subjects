95bcc5fc2a088d9db667c2ed308a6abf9b80e7ae
Alexey <asdalexey@yandex.ru>
2018-05-23 22:30:18 +0300

    #3 basic event bath

--- src/index.js
+++ src/index.js
@@ -4,40 +4,23 @@ import { Subject } from 'rxjs/Subject';
 
 const subject = new Subject();
 
-const observable = interval(1000).pipe(take(5));
-
 const observerA = {
   next: (x) => (console.log('A next ', x)),
   error: (err) => (console.log('A error ', err)),
   complete: () => (console.log('A done')),
 };
+subject.subscribe(observerA);
 
 const observerB = {
   next: (x) => (console.log('B next ', x)),
   error: (err) => (console.log('B error ', err)),
   complete: () => (console.log('B done')),
 };
-
-/*
-const bridgeObserver = {
-  observers: [],
-  next: function (x) {
-    this.observers.forEach((o) => o.next(x));
-  },
-  error: function (err) {
-    this.observers.forEach((o) => o.error(err));
-  },
-  complete: function (err) {
-    this.observers.forEach((o) => o.complete());
-  },
-  subscribe: function (observer) {
-    this.observers.push(observer);
-  },
-};
-*/
-// observable.subscribe(observerA);
-
-observable.subscribe(subject);
-
-subject.subscribe(observerA);
 setTimeout(() => (subject.subscribe(observerB)), 2000);
+
+subject.next(1);
+subject.next(2);
+subject.next(3);
+
+// subject.error('error');
+// setInterval(() => (subject.next(10)), 1000);
