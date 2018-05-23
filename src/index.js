3b1704076d8a1ed37d591011184d0e550f16bdeb
Alexey <asdalexey@yandex.ru>
2018-05-23 22:36:34 +0300

    #5 BehaviorSubject

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { interval } from 'rxjs/observable/interval';
 import { take } from 'rxjs/operators/take';
 import { Subject } from 'rxjs/Subject';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
-const subject = new Subject();
+// const subject = new Subject();
+const subject = new BehaviorSubject(0);
 
 const observerA = {
   next: (x) => (console.log('A next ', x)),
@@ -10,17 +12,23 @@ const observerA = {
   complete: () => (console.log('A done')),
 };
 subject.subscribe(observerA);
+console.log('observer A subscribed!');
 
 const observerB = {
   next: (x) => (console.log('B next ', x)),
   error: (err) => (console.log('B error ', err)),
   complete: () => (console.log('B done')),
 };
-setTimeout(() => (subject.subscribe(observerB)), 2000);
 
 subject.next(1);
 subject.next(2);
 subject.next(3);
 
+setTimeout(() => {
+  subject.subscribe(observerB);
+  console.log('observer B subscribed!');
+}, 2000);
+
 // subject.error('error');
+// setInterval(() => (subject.next(10)), 1000);
 // setInterval(() => (subject.next(10)), 1000);
