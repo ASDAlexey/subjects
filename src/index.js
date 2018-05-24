0b426b8cc849fe9713fdf0dfe7cff7078ec71a12
Alexey <asdalexey@yandex.ru>
2018-05-24 22:41:29 +0300

    #6 ReplaySubject

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import { interval } from 'rxjs/observable/interval';
 import { take } from 'rxjs/operators/take';
 import { Subject } from 'rxjs/Subject';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
 
 // const subject = new Subject();
-const subject = new BehaviorSubject(0);
+// const subject = new BehaviorSubject(0);
+// const subject = new ReplaySubject(Number.POSITIVE_INFINITY, 250); // 1000 - how long time ReplaySubject should store data
+const subject = new ReplaySubject(100); // analog BehaviorSubject subject - store only last value
 
 const observerA = {
   next: (x) => (console.log('A next ', x)),
@@ -20,14 +23,15 @@ const observerB = {
   complete: () => (console.log('B done')),
 };
 
-subject.next(1);
-subject.next(2);
-subject.next(3);
+setTimeout(() => (subject.next(1)), 100);
+setTimeout(() => (subject.next(2)), 200);
+setTimeout(() => (subject.next(3)), 300);
+setTimeout(() => (subject.complete()), 350);
 
 setTimeout(() => {
   subject.subscribe(observerB);
   console.log('observer B subscribed!');
-}, 2000);
+}, 400);
 
 // subject.error('error');
 // setInterval(() => (subject.next(10)), 1000);
