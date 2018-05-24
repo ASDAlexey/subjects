0bc70285e633dea3aa0147e8391953ad5b370421
Alexey <asdalexey@yandex.ru>
2018-05-24 22:59:10 +0300

    #7 multicast

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { interval } from 'rxjs/observable/interval';
 import { take } from 'rxjs/operators/take';
+import { multicast } from 'rxjs/operators/multicast';
 import { Subject } from 'rxjs/Subject';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
@@ -14,14 +15,24 @@ import { AsyncSubject } from 'rxjs/AsyncSubject';
 // AsyncSubject: replays one, only if completed
 
 // const subject = new ReplaySubject(100); // analog BehaviorSubject subject - store only last value
-const subject = new AsyncSubject();
+// const subject = new AsyncSubject();
+
+const connectableObservale = interval(1000).pipe(
+  take(5),
+  // multicast(new Subject()),
+  multicast(new ReplaySubject(100)),
+);
 
 const observerA = {
   next: (x) => (console.log('A next ', x)),
   error: (err) => (console.log('A error ', err)),
   complete: () => (console.log('A done')),
 };
-subject.subscribe(observerA);
+
+connectableObservale.connect();
+
+// subject.subscribe(observerA);
+connectableObservale.subscribe(observerA);
 console.log('observer A subscribed!');
 
 const observerB = {
@@ -30,15 +41,19 @@ const observerB = {
   complete: () => (console.log('B done')),
 };
 
-setTimeout(() => (subject.next(1)), 100);
-setTimeout(() => (subject.next(2)), 200);
-setTimeout(() => (subject.next(3)), 300);
-setTimeout(() => (subject.complete()), 350);
+// observable.subscribe(subject);
+
+// setTimeout(() => (subject.next(1)), 100);
+// setTimeout(() => (subject.next(2)), 200);
+// setTimeout(() => (subject.next(3)), 300);
+// setTimeout(() => (subject.complete()), 350);
 
 setTimeout(() => {
-  subject.subscribe(observerB);
+  // subject.subscribe(observerB);
+  connectableObservale.subscribe(observerB);
   console.log('observer B subscribed!');
-}, 400);
+  // }, 400);
+}, 2000);
 
 // subject.error('error');
 // setInterval(() => (subject.next(10)), 1000);
